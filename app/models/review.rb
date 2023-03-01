c665847f4ae36baebb04fa32ce313a7e15a6bed2
amsun2 <amsun0@chicagobooth.edu>
2023-03-01 23:02:41 +0000

    Added Review resource

---
+++ app/models/review.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id         :integer          not null, primary key
+#  commentary :text
+#  rating     :float
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  book_id    :integer
+#  user_id    :integer
+#
+class Review < ApplicationRecord
+  belongs_to(:book, { :required => true, :class_name => "Book", :foreign_key => "book_id" })
+
+  belongs_to(:user, { :required => true, :class_name => "User", :foreign_key => "user_id" })
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_01_230034) do
+ActiveRecord::Schema.define(version: 2023_03_01_230126) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2023_03_01_230034) do
   create_table "genres", force: :cascade do |t|
     t.integer "book_id"
     t.string "genre"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "user_id"
+    t.float "rating"
+    t.text "commentary"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
